a0fe4480c1e2253e4d5852fda17eab9d37dd4c8b
Muhammad Khusni Fikri <113235525+mkfikri24@users.noreply.github.com>
2024-07-15 10:23:39 +0700

    Create Indonesia-Severe_Food.sql

---
+++ SQL/Indonesia-Severe_Food.sql
@@ -0,0 +1,19 @@
+SELECT
+  a.country_name,
+  a.year,
+  b.series_code,
+  a.indicator_name,
+  a.value
+FROM
+  worldbank_wdi.data AS a
+INNER JOIN
+  worldbank_wdi.series_description AS b
+ON
+  a.indicator_code = b.series_code
+WHERE
+  a.country_name = 'Indonesia'
+  AND b.series_code LIKE '%SVFI%'
+  AND b.long_definition LIKE '%food%'
+  AND b.indicator_name LIKE '%severe%'
+ORDER BY
+  a.year asc
